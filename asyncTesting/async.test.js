c7c9381fea3fa4e10e62edf10e1da401a5b67d92
sunnykumar.styre@gmail.com <sunnykumar.styre@gmail.com>
2022-05-22 15:37:18 +0200

    formating the file

--- asyncTesting/async.test.js
+++ asyncTesting/async.test.js
@@ -1,8 +1,8 @@
 const fetchData = require('./async');
 
-describe("testing todo",()=>{
-    it("Should return correct todo",async()=>{
+describe("testing todo", () => {
+    it("Should return correct todo", async () => {
         const result = await fetchData(1);
-        expect(result.userId).toEqual(1);        
+        expect(result.userId).toEqual(1);
     })
 })
