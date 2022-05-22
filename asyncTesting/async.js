80d65374237327220e3f6f851f430e33a64baf41
sunnykumar.styre@gmail.com <sunnykumar.styre@gmail.com>
2022-05-22 15:24:07 +0200

    updating the aynsc file for exports

--- asyncTesting/async.js
+++ asyncTesting/async.js
@@ -3,11 +3,10 @@ const axios = require('axios');
 const fetchData =async (id)=>{
     try{
         const result = await axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`);
-        console.log(result.data);
-        //return 
+        return result.data
     }catch(e){
         throw new Error(e);
     }
 }
-fetchData(1);
-//module.exports =  ;
+
+module.exports = fetchData;
